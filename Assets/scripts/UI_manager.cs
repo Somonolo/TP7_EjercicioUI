9ba24a9ebcd943c574501163880ffff26aa35c95
Somonolo <simonfejler@gmail.com>
2026-04-24 14:18:13 -0300

    Initial commit

---
+++ Assets/scripts/UI_manager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_manager : MonoBehaviour
+{
+    public TMP_InputField inputNumero;
+    public TextMeshProUGUI txtResultado;
+
+    private int numeroActual;
+    private bool numeroCargado = false;
+    private string ultimoValorIngresado;
+
+    void Start()
+    {
+        txtResultado.gameObject.SetActive(false);
+    }
+
+    public void MenosUno()
+    {
+        if (inputNumero.text != ultimoValorIngresado)
+        {
+            if (inputNumero.text != "")
+            {
+                numeroActual = int.Parse(inputNumero.text);
+                ultimoValorIngresado = inputNumero.text;
+                numeroCargado = true;
+            }
+            else
+            {
+                txtResultado.text = "Ingresa un numero";
+                txtResultado.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        if (numeroActual > 0)
+        {
+            numeroActual--;
+        }
+
+        txtResultado.text = "Resultado: " + numeroActual;
+        txtResultado.gameObject.SetActive(true);
+    }
+}
